027f4ca1c869b589466ae1628497dc9476bafe18
japanjoshi <japanjoshi1234@gmail.com>
2024-06-19 23:19:03 +0530

    for pull

--- lib/screens/ui/intro/enter_basic-2.dart
+++ lib/screens/ui/intro/enter_basic-2.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
-
 import '../../tab_bar.dart';
 
 class BasicDetailsPage2 extends StatefulWidget {
