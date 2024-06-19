e357849106736a35cd52816e4ab4a69b8d0faa19
japanjoshi <japanjoshi1234@gmail.com>
2024-06-19 23:18:10 +0530

    created SHA 1 and SHA 256, also key.properties

--- lib/screens/ui/intro/enter_basic-2.dart
+++ lib/screens/ui/intro/enter_basic-2.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:jb_user_app/screens/home_screen.dart';
 
 import '../../tab_bar.dart';
 
